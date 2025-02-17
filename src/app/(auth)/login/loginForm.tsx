4049a068cb95cdf4e84b9d90da8f15cea6c0c476
umm e habiba <161445850+UmmeHabiba1312@users.noreply.github.com>
2025-02-17 22:26:52 +0500

    Update style/loginForm

--- src/app/(auth)/login/loginForm.tsx
+++ src/app/(auth)/login/loginForm.tsx
@@ -1,26 +1,31 @@
-'use client';
+'use client'
 
-import React from 'react';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import SubmitButton from '@/components/ui/submitButton';
-import { logIn } from '@/lib/auth';
-import { FcGoogle } from 'react-icons/fc';
-import Link from 'next/link';
+import React from 'react'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import SubmitButton from '@/components/ui/submitButton'
+import { logIn } from '@/lib/auth'
+import { FcGoogle } from 'react-icons/fc'
+import Link from 'next/link'
 
 const LoginForm = () => {
-  const [state, action] = React.useActionState(logIn, undefined);
+  const [state, action] = React.useActionState(logIn, undefined)
 
   return (
-    <form action={action} className="space-y-4 h-auto bg-white w-full max-w-md mx-auto  p-9 rounded-lg border-[1px] border-gray-300 ">
+    <form
+      action={action}
+      className="space-y-4 h-auto bg-white w-full max-w-md mx-auto  p-9 rounded-lg border-[1px] border-gray-200 "
+    >
       {/* Error Message */}
       {state?.error?.message && (
-        <p className="text-xs sm:text-sm bg-red-500 text-white p-2 rounded">{state.error.message}</p>
+        <p className="text-xs sm:text-sm bg-red-500 text-white p-2 rounded">
+          {state.error.message}
+        </p>
       )}
 
       {/* Email Input */}
       <div>
-        <Label htmlFor="email" className="block text-sm font-medium tracking-wide text-gray-600">
+        <Label htmlFor="email" className="block text-sm font-medium tracking-wide text-gray-800">
           Email
         </Label>
         <Input
@@ -28,14 +33,16 @@ const LoginForm = () => {
           name="email"
           type="email"
           placeholder="Enter your email"
-          className="w-full mt-1 px-2 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
+          className="w-full mt-1 px-2 py-5 border border-gray-200 rounded-md focus:ring-blue-500 focus:border-blue-500"
         />
       </div>
-      {state?.error?.email && <p className="text-xs sm:text-sm text-red-500">{state.error.email}</p>}
+      {state?.error?.email && (
+        <p className="text-xs sm:text-sm text-red-500">{state.error.email}</p>
+      )}
 
       {/* Password Input */}
       <div>
-        <Label htmlFor="password" className="block text-sm font-medium tracking-wide text-gray-600">
+        <Label htmlFor="password" className="block text-sm font-medium tracking-wide text-gray-800">
           Password
         </Label>
         <Input
@@ -43,7 +50,7 @@ const LoginForm = () => {
           name="password"
           type="password"
           placeholder="Enter your password"
-          className="w-full mt-1 px-2 py-2 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 mb-1"
+          className="w-full mt-1 px-2 py-5 border border-gray-200 rounded-md focus:ring-blue-500 focus:border-blue-500 mb-1"
         />
         <div className="text-right mt-1">
           <Link href="#" className="text-xs sm:text-sm text-blue-400 hover:underline">
@@ -75,13 +82,13 @@ const LoginForm = () => {
       {/* Google Sign-in Button */}
       <Link
         href="/"
-        className="flex items-center tracking-wide  hover:bg-gray-100 justify-center gap-2 font-medium text-xs sm:text-sm bg-white text-gray-500 py-2 w-full rounded-md border-[1px] border-gray-300 disabled:opacity-50 disabled:cursor-not-allowed"
+        className="flex items-center tracking-wide  hover:bg-gray-100 justify-center gap-2 font-medium text-xs sm:text-sm bg-white text-gray-500 py-[10px] w-full rounded-md border-[1px] border-gray-200 disabled:opacity-50 disabled:cursor-not-allowed"
       >
         <FcGoogle className="text-xl" />
         Continue With Google
       </Link>
     </form>
-  );
-};
+  )
+}
 
-export default LoginForm;
+export default LoginForm
